9764db28ec9abaab056e5fb61985091a1e6f6354
rupi502021 <77380567+rupi502021@users.noreply.github.com>
2021-03-08 01:31:58 +0200

    Create makeable project
    שיהיה לנו בהצלחה

---
+++ MakeAble/MakeAble/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MakeAble.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MakeAble/MakeAble/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MakeAble.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
