991d4802537251b4f756dab26a19ee797bdddc19
Niv Ovadia <niv.ovadia25@gmail.com>
2021-03-09 19:34:46 +0200

    הוספת 3 מחלקות
    הוספת 3 מחלקות: הזמנות, כלים והודעות

---
+++ MakeAble/MakeAble/Models/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MakeAble.Models
+{
+    public class Message
+    {
+        int messageId;
+        DateTime date;
+        DateTime time;
+        string description;
+        string urlPhoto;
+        string urlVideo;
+
+        public Message(int messageId, DateTime date, DateTime time, string description, string urlPhoto, string urlVideo)
+        {
+            this.messageId = messageId;
+            this.date = date;
+            this.time = time;
+            this.description = description;
+            this.urlPhoto = urlPhoto;
+            this.urlVideo = urlVideo;
+        }
+
+        public Message() { }
+
+        public int MessageId { get => messageId; set => messageId = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public DateTime Time { get => time; set => time = value; }
+        public string Description { get => description; set => description = value; }
+        public string UrlPhoto { get => urlPhoto; set => urlPhoto = value; }
+        public string UrlVideo { get => urlVideo; set => urlVideo = value; }
+    }
+}
---
+++ MakeAble/MakeAble/Models/tool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MakeAble.Models
+{
+    public class Tool
+    {
+        int toolId;
+        string model;
+        string brand;
+        bool qualifications;
+        string description;
+
+        public Tool(int toolId, string model, string brand, bool qualifications, string description)
+        {
+            this.toolId = toolId;
+            this.model = model;
+            this.brand = brand;
+            this.qualifications = qualifications;
+            this.description = description;
+        }
+
+        public Tool() { }
+
+        public int ToolId { get => toolId; set => toolId = value; }
+        public string Model { get => model; set => model = value; }
+        public string Brand { get => brand; set => brand = value; }
+        public bool Qualifications { get => qualifications; set => qualifications = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
