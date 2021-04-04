98c2bc24b64360a4b5c4aaf249f443616f799023
rupi502021 <77380567+rupi502021@users.noreply.github.com>
2021-04-04 18:30:14 +0300

    בלה

---
+++ MakeAble/MakeAble/obj/Release/Package/PackageTmp/Scripts/ajaxCallsWithKey.js
@@ -0,0 +1,15 @@
+﻿function ajaxCall(method, api, data, successCB, errorCB) {
+    $.ajax({
+        type: method,
+        url: api,
+        data: data,
+        cache: false,
+		headers: {
+            'resource_id': '5c78e9fa-c2e2-4771-93ff-7f400a12f7ba',
+        },
+        contentType: "application/json",
+        dataType: "json",
+        success: successCB,
+        error: errorCB
+    });
+}
